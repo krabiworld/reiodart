8ff22f933ac347246cbfcc0d6446ba5b10771245
mineejo <evgenyshaimov@gmail.com>
2023-02-03 18:29:46 +0300

    Added comments and a useful check for proxies.

--- lib/src/widget/store/store.dart
+++ lib/src/widget/store/store.dart
@@ -10,21 +10,33 @@ import '../widget_watcher.dart';
 class ReioProxy {
   dynamic value;
 
+  /// Creates [ReioProxy] with an initial value,
+  /// it usually should not be empty unless destroyed.
   ReioProxy(this.value);
 
   final ReioDependency dependency = ReioDependency();
 
+  /// Standard method for getting a value,
+  /// takes a widget node to set its [dependency].
   dynamic get(ReioNodeWidget node) {
+    if (value == null) return;
+
     dependency.depend(node);
     return value;
   }
 
+  /// Standard method for setting the value,
+  /// accepts the value and notifies
+  /// [dependency] of the update.
   void set(dynamic newValue) {
     if (value == newValue) return;
+
     value = newValue;
     dependency.notify();
   }
 
+  /// Destroys value and [dependency],
+  /// thereby destroying [ReioProxy].
   void destroy() {
     value = null;
     dependency.destroy();
