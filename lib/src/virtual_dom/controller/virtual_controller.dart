1ae0a973d3f3183c64a0dfe768f6aed19c030cd0
mineejo <evgenyshaimov@gmail.com>
2023-02-04 21:19:11 +0300

    Added copyright.

--- lib/src/virtual_dom/controller/virtual_controller.dart
+++ lib/src/virtual_dom/controller/virtual_controller.dart
@@ -1,3 +1,8 @@
+// Copyright (c) MineEjo.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
 import 'dart:html';
 import 'dart:math';
 
