c73cd1dfe0260bbaaed097248edc2c38aa8a1b4e
mineejo <evgenyshaimov@gmail.com>
2023-02-05 18:23:02 +0300

    Added import for comments.

--- lib/src/widget/html/html.dart
+++ lib/src/widget/html/html.dart
@@ -3,6 +3,7 @@
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the root directory of this source tree.
 
+import 'element/html_element.dart';
 import 'element/tag/element_tag_br.dart';
 import 'element/tag/element_tag_wbr.dart';
 
