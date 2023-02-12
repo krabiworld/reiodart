ff1af6b60ec2e76c851ee32c16c2825ddebe201a
mineejo <evgenyshaimov@gmail.com>
2023-02-12 11:36:28 +0300

    Added license notification.

--- lib/reio.dart
+++ lib/reio.dart
@@ -1,3 +1,8 @@
+// Copyright (c) MineEjo.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
 /// Contains the current information
 /// or configuration of the installed version.
 class Reio {
