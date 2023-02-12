ea8fce6cc9641cc18766b8600a45691dad503ee1
mineejo <evgenyshaimov@gmail.com>
2023-02-12 19:59:39 +0300

    Removed the remnants of routing from the core.

--- core/lib/src/widget/html/html.dart
+++ core/lib/src/widget/html/html.dart
@@ -3,21 +3,9 @@
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the root directory of this source tree.
 
-import 'dart:html';
-
 import 'element/html_element.dart';
 import 'element/tag/element_tag_br.dart';
 import 'element/tag/element_tag_wbr.dart';
-
-/// Contains the name of the custom event
-/// that is triggered if the URL
-/// of the page was changed using the router.
-final String routerEvent = 'reio-router-event';
-
-/// Binds the [function] to a router event.
-void onRoute(Function() function) {
-  window.addEventListener(routerEvent, (event) => function.call());
-}
 
 const String _startJumpConstruction = '[jump:';
 const String _endJumpConstruction = ']';
