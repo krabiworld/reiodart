5682d547f10113d71cdbcae8dbb9d5e602f3e710
mineejo <evgenyshaimov@gmail.com>
2023-02-12 11:35:47 +0300

    Separated file exports.

--- lib/widget_stores.dart
+++ lib/widget_stores.dart
@@ -3,5 +3,6 @@
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the root directory of this source tree.
 
-// widget
-export 'src/widget/widget.dart';
+// widget/store
+export 'src/widget/store/store_writable.dart';
+export 'src/widget/store/store_readable.dart';
