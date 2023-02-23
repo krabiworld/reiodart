0ee70ceca96962e5870171ad3a2f1a358a8a26be
mineejo <evgenyshaimov@gmail.com>
2023-02-23 15:41:09 +0300

    Unnecessary code removed.

--- core/lib/src/widget/html/element/tag/element_tag_dialog.dart
+++ core/lib/src/widget/html/element/tag/element_tag_dialog.dart
@@ -3,8 +3,6 @@
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the root directory of this source tree.
 
-import 'dart:html';
-
 import '../../../../virtual_dom/virtual_attr.dart';
 import '../html_element.dart';
 import '../html_element_visible.dart';
@@ -13,8 +11,6 @@ import '../html_element_visible.dart';
 // are not and will not be implemented. If you notice this,
 // send it as a issue to https://github.com/MineEjo/reiodart/issues
 // Adapted for HTML version 5.
-
-const String _dialogData = 'data-reio-dialog';
 
 /// Contains [WidgetElement] that contains a virtual node
 /// with the `<dialog>` tag.
@@ -25,7 +21,6 @@ class Dialog extends WidgetElementVisible {
   /// [Read more...](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/dialog)
   Dialog([super.value]) {
     tag = 'dialog';
-    node.attrs!.add(VirtualAttr(_dialogData, node.number));
   }
 
   /// Indicates that the dialog is active
@@ -36,30 +31,6 @@ class Dialog extends WidgetElementVisible {
       VirtualAttr attr = VirtualAttr('open', '');
       node.attrs!.add(attr);
     }
-    return this;
-  }
-
-  /// Shows or closes the modal dialog window.
-  /// [Read more...](https://developer.mozilla.org/en-US/docs/Web/API/HTMLDialogElement/showModal)
-  Dialog modal(bool boolean,
-      {String? returnValue,
-      String? closeValue,
-      Function(DialogElement)? onModal,
-      Function(DialogElement)? onClose}) {
-    DialogElement? element = document
-        .querySelector('[$_dialogData="${node.number}"]') as DialogElement?;
-    if (element == null) return this;
-
-    if (returnValue != null) element.returnValue = returnValue;
-
-    if (boolean) {
-      element.showModal();
-      onModal?.call(element);
-    } else if (element.open == true) {
-      element.close(closeValue);
-      onClose?.call(element);
-    }
-
     return this;
   }
 }
