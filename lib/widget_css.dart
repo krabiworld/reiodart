c7360193650426f1a30d6ed1ed1f3d6a640990da
mineejo <evgenyshaimov@gmail.com>
2023-03-03 20:11:20 +0300

    Removed CSS package.

--- lib/widget_css.dart
+++
@@ -1,8 +0,0 @@
-// Copyright (c) MineEjo.
-//
-// This source code is licensed under the MIT license found in the
-// LICENSE file in the root directory of this source tree.
-
-// widget/css
-export 'src/widget/css/css.dart';
-export 'src/widget/css/css_root.dart' show Theme;
--- web/index.widgets.dart
+++ web/index.widgets.dart
@@ -1,10 +1,8 @@
 import 'dart:html';
 
-// import 'widgets/index.dart';
-// import 'widgets/layout_router.dart';
-// import 'widgets/ofdex.dart';
-
-import 'widgets/layout_css.dart';
+import 'widgets/index.dart';
+import 'widgets/layout_router.dart';
+import 'widgets/ofdex.dart';
 
 String pathRoot = '/';
 String pathIndex = '${pathRoot}index.html';
@@ -14,10 +12,8 @@ RegExp routeIndex = RegExp(r'(/$)|(/index.html$)');
 RegExp routeOfdex = RegExp(r'/ofdex.html$');
 
 void widgetConstruction() {
-  // layoutRouter.inject(document.getElementById('app'));
-  //
-  // index.toRoute(Layout.indexSlot, routeIndex, lazy: true);
-  // ofdex.toRoute(Layout.ofdexSlot, routeOfdex, lazy: true);
+  layoutRouter.inject(document.getElementById('app'));
 
-  layoutCss.inject(document.getElementById('app'));
+  index.toRoute(Layout.indexSlot, routeIndex, lazy: true);
+  ofdex.toRoute(Layout.ofdexSlot, routeOfdex, lazy: true);
 }
